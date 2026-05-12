c77229046d48c6d7d20bc1f4d23695da64519dce
kevin Rivera <kriverarojas44@gmail.com>
2026-05-12 16:08:33 -0500

    cambio

--- app/Filament/Resources/Products/Tables/ProductsTable.php
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\Products\Tables;
 
 use Filament\Actions\Action;
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
@@ -93,6 +94,7 @@ class ProductsTable
             ->recordActions([
                 // ViewAction::make(),
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
